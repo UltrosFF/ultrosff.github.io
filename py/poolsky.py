dcc4d23beef7b1cd888b32f73ee6cea2c0a2d1a6
UltrosFF <129062444+UltrosFF@users.noreply.github.com>
2023-06-26 18:37:41 +0200

    Update poolsky.py

--- py/poolsky.py
+++ py/poolsky.py
@@ -812,8 +812,8 @@ def pool_assets(pool_id):
     retry_delay = 15
     
     # Stream addresses in batches of 250
-    for i in range(0, len(addresses), 250):
-        batch_addresses = addresses[i:i+250]
+    for i in range(0, len(addresses), 1000):
+        batch_addresses = addresses[i:i+1000]
         
         data = {
             "_stake_addresses": batch_addresses
